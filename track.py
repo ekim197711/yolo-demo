ebe76ef49ab76faa18b871f6792972bf23d51b35
Mike Nielsen <mmni@protonmail.com>
2023-12-19 22:29:35 +0100

    More files

--- track.py
+++ track.py
@@ -5,5 +5,5 @@ model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
 # model = YOLO('path/to/best.pt')  # load a custom model
 
 # Track with the model
-results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True)
+results = model.track(source="https://www.youtube.com/watch?v=Xdzuze5XSXE&ab_channel=MikeM%C3%B8llerNielsen", show=True)
 # results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml")
